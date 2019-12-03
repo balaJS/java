330fcd2da45c9a355688eb4e300a409084d31924
balaJS <vvbala1995@gmail.com>
2019-12-03 06:35:25 +0530

    done

--- Java/oops/1/Shape/Circle.java
+++ Java/oops/1/Shape/Circle.java
@@ -1,4 +1,5 @@
 class Circle extends Shape {
+	public int distance = 12;
 
 	public float area(float radius) {
 		float PI = 22/7;
--- Java/oops/1/Shape/Squre.java
+++ Java/oops/1/Shape/Squre.java
@@ -1,5 +1,6 @@
 class Squre extends Shape {
-
+	public int distance = 10;
+	
 	public float area(float x) {
 		return x * x;
 	}
